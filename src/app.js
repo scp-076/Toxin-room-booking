0d98e011cdf02528192f8935d7a3127b47fc5598
Михаил Евсюнин <ss.offensive@gmail.com>
2020-06-10 13:01:37 +0300

    range date input created, minor refactoring

--- src/app.js
+++ src/app.js
@@ -4,5 +4,6 @@ import './theme/vendor.js';
 import './theme/form-elements/dropdown_rooms/dropdown_rooms.js';
 import './theme/form-elements/dropdown_guests/dropdown_guests.js';
 import './theme/form-elements/dropdown_date/dropdown_date.js';
+import './theme/form-elements/dropdown_date_filtered/dropdown_date_filtered.js';
 import './theme/form-elements/masked_date_input/masked_date_input.js';
 
